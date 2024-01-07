40bae1345fba8101f7b33c8354a39e5eb6dfae8b
Pavel Francírek <pavel@toplist.cz>
2024-01-07 19:11:32 +0100

    fastapi.TestClient

--- test/test_main.py
+++ test/test_main.py
@@ -1,9 +1,14 @@
 import pytest
-import requests
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from fastapi.testclient import TestClient
+import main
 
-URL = "http://localhost:9000/"
+client = TestClient(main.app)
 
 def test_root():
-    response = requests.get(URL)
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    message = response.json()["message"]
+    assert len(message) < 40
