8451e477f1392c0d6022a263ce1f963b5b82d790
Pavel Francírek <pavel@toplist.cz>
2024-01-08 16:37:33 +0100

    Alembic (#1)
    * WIP
    
    * add alembic migration
    
    * set variables to $GITHUB_ENV

--- models.py
+++ models.py
@@ -1,10 +1,19 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
+from db import engine
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, Identity
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
 
-class User(SQLModel):
-    id: int
-    email: str
+class Base(DeclarativeBase):
+    pass
 
-class UpVote(SQLModel):
-    id: int
-    sid: str = Field(nullable=False)
+class Users(Base):
+    __tablename__: str = "users"
+
+    id: Mapped[int] = Column(Integer, Identity(always=True), primary_key=True, index=True)
+    email: Mapped[str] = Column(String, unique=True, nullable=False)
+
+class UpVotes(Base):
+    __tablename__: str = "upvotes"
+
+    id: Mapped[int] = Column(Integer, Identity(always=True), primary_key=True, index=True)
+    sid: Mapped[str] = Column(Text, unique=True, nullable=False)
